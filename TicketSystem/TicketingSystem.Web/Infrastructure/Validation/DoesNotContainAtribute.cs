6e6aa7da0498d51ca003ea6af115666a8fb62913
KalinaMitova <kalina.mitova@gmail.com>
2018-03-27 12:32:43 +0300

    commit

---
+++ TicketSystem/TicketingSystem.Web/Infrastructure/Validation/DoesNotContainAtribute.cs
@@ -0,0 +1,34 @@
+﻿
+namespace TicketingSystem.Web.Infrastructure.Security
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DoesNotContainAtribute : ValidationAttribute
+    {
+        private string word;
+
+        public DoesNotContainAtribute(string word)
+        {
+            this.word = word;
+            this.ErrorMessage = "{0} should not contain the word " + word;
+        }
+
+        public override bool IsValid(object value)
+        {
+            string valueAsString = value as string;
+            if (valueAsString == null)
+            {
+                throw new ArgumentException("Does not contain attribute not set on string property!");
+            }
+
+            if (!valueAsString.Contains(this.word))
+            {
+                return true;
+            }
+
+            return false;
+        } 
+    }
+}
---
+++ TicketSystem/TicketingSystem.Web/ViewModels/Tickets/ListTicketViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace TicketingSystem.Web.ViewModels.Tickets
+{
+    using AutoMapper;
+
+    using TicketingSystem.Models;
+    using TicketingSystem.Web.Infrastructure.Mapping;
+
+    public class ListTicketViewModel : IMapFrom<Ticket>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Priority { get; set; }
+
+        public string TicketTitle { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public void CreateMappings(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<Ticket, ListTicketViewModel>()
+                .ForMember(m => m.TicketTitle, opt => opt.MapFrom(t => t.Title))
+                .ForMember(m => m.CategoryName, opt => opt.MapFrom(t => t.Category.Name))
+                .ForMember(m => m.AuthorName, opt => opt.MapFrom(t => t.Author.UserName))
+                .ForMember(m => m.Priority, opt => opt.MapFrom(t => t.Priority.ToString()))
+                .ReverseMap();
+        }
+    }
+}
